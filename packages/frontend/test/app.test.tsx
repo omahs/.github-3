162065e81d21c5de799f652aa0c41415ff43ac2c
Wilhelm Thieme <wjthieme@gmail.com>
2022-08-08 07:50:54 +0200

    Updated build tools and fixed jest

--- packages/frontend/test/app.test.tsx
+++ packages/frontend/test/app.test.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from '../src/components/App';
+import React from "react";
+import renderer from "react-test-renderer";
+import App from "../src/components/App";
 
-it('App should be displayed', () => {
+it("App should be displayed", () => {
     const component = renderer.create(
-      <App/>,
+        <App/>,
     );
-    let tree = component.toJSON();
+    const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
