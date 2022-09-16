2a55640a9a175c07884547851e21c95ec666962a
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-09-16 17:47:44 +0200

    Added CORS to the backend package (#81)
    * Moved the API model to the core module
    
    * Added Json Schemas to the model
    
    * Added CORS to the backend package
    
    * lint fix

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import swaggerUi from "swagger-ui-express";
 import morgan from "morgan";
 import dotenv from "dotenv";
+import cors from "cors";
 import { RegisterRoutes } from "./modules/routes.gen.js";
 import { RegisterCronsJobs } from "./modules/cron.js";
 import { HttpError } from "./modules/error.js";
@@ -22,6 +23,7 @@ app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("./public"));
+app.use(cors());
 
 RegisterCronsJobs(app);
 RegisterRoutes(app);
@@ -33,8 +35,8 @@ app.use((req: Request) => {
     throw new HttpError(404, `"${req.url}" is not found.`);
 });
 
-app.use((err: any, _: Request, res: Response) => {
-    res.status(err.status || 500).send(err.message.toString());
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    res.status(err.status || 500).send(err.message);
 });
 
 app.listen(process.env.PORT);
