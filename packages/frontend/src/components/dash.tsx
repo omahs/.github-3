b038227c356c42988dae738f0e5dcf5406be38a6
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-09-23 09:26:01 +0200

    Added a structure to show Tiles on the dashboard page (#123)
    * Added a structure to show Tiles on the dashboard page
    
    * Added styling for tiles
    
    * Fixed eslint

--- packages/frontend/src/components/dash.tsx
+++ packages/frontend/src/components/dash.tsx
@@ -1,32 +1,48 @@
 import "../styles/dash.css";
-import React, { Component } from "react";
+import React, { Component, ReactNode } from "react";
 import { withAuth0, WithAuth0Props } from "@auth0/auth0-react";
-import { IDashboardOverviewResponse } from "core";
-import { getDashboardOverview } from "../modules/api";
+import { decodeJwt } from "jose";
 
 interface IState {
-    overview?: IDashboardOverviewResponse;
-    error?: string;
+    tiles: Array<ReactNode>;
 }
+
+const defaultTiles: Array<ReactNode> = [
+    <div key="abc" className="tile">1</div>,
+    <div key="def" className="tile">2</div>,
+    <div key="ghi" className="tile">3</div>,
+    <div key="jkl" className="tile">4</div>
+];
+
+const adminTiles: Array<ReactNode> = [
+    <div key="mno" className="tile">1</div>,
+    <div key="pqr" className="tile">2</div>,
+    <div key="stu" className="tile">3</div>,
+    <div key="vwx" className="tile">4</div>
+];
 
 class Dash extends Component<WithAuth0Props, IState> {
 
     constructor(props: WithAuth0Props) {
         super(props);
-        this.state = { };
+        this.state = { tiles: defaultTiles };
     }
 
     componentDidMount() {
         this.props.auth0.getAccessTokenSilently()
-            .then(getDashboardOverview)
-            .then(overview => this.setState({ overview }))
-            .catch(_ => this.setState({ error: "Error" }));
+            .then(token => {
+                const claim = decodeJwt(token);
+                const roles = claim.permissions as Array<string>;
+                if (roles.includes("admin")) {
+                    this.setState({ tiles: defaultTiles.concat(adminTiles) });
+                }
+            });
     }
 
     render() {
         return (
             <div className="dash">
-                {`Dash\n${this.state.error}\n${this.state.overview?.cumlative}`}
+                {this.state.tiles}
             </div>
         );
     }
