bc288ec6b78d79fbf8ec9ee7d2b6255dcedd1115
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-11-02 10:37:15 +0100

    Added slow-down soft rate limiting (#216)
    * Added slow-down soft rate limiting
    
    * lint fix

--- packages/backend/src/modules/security.ts
+++ packages/backend/src/modules/security.ts
@@ -1,8 +1,12 @@
-import cors from "cors";
 import { Application } from "express";
+import cors from "cors";
+import slowDown from "express-slow-down";
 
 export const RegisterSecurityMiddleware = (app: Application) => {
     app.use(cors());
-
-    //TODO: rate limit?
+    app.use(slowDown({
+        windowMs: 60,
+        delayAfter: 100,
+        delayMs: 500
+    }));
 };
