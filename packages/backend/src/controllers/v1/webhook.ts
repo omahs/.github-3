b12718a5c504d8b02070529504d38d937ea89d78
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-11-02 10:58:15 +0100

    Ignore Coinbase notifications that do not relate to jewel.cash by just returning 204 (#218)
    * Ignore webhook notifications that do not relate to jewel.cash by just returning 204
    
    * Lint fix

--- packages/backend/src/controllers/v1/webhook.ts
+++ packages/backend/src/controllers/v1/webhook.ts
@@ -2,7 +2,6 @@ import { BigNumber } from "bignumber.js";
 import { Body, Hidden, Post, Route, Security, SuccessResponse } from "tsoa";
 import { Payment, PendingPayment } from "../../entities/payment.js";
 import { getExchangeRate } from "../../modules/coinbase.js";
-import { HttpError } from "../../modules/error.js";
 
 @Route("/v1/webhook")
 export class WebhookController {
@@ -14,13 +13,14 @@ export class WebhookController {
     public async receivedCoinbaseWebhook(@Body() body: any): Promise<void> {
         if (body.type === "wallet:addresses:new-payment") {
             const id: string = body.data.name ?? "";
+            if (id == "") { return; }
             const createdDate: string = body.created_at ?? "";
             const timestamp = new Date(createdDate).toUnix();
             const currency: string = body.additional_data.amount.currency ?? "";
             const amount = new BigNumber(body.additional_data.amount.amount as string);
             const transactionHash: string = body.additional_data.hash ?? "";
             const pending = await PendingPayment.findById(id);
-            if (pending == null) { throw new HttpError(404, `No pending payment found for ${id}.`);}
+            if (pending == null) { return; }
             const exchangeRate = await getExchangeRate(currency, timestamp);
             const usdEquivalent = amount.multipliedBy(exchangeRate);
             const fee = usdEquivalent.multipliedBy(0.15);
