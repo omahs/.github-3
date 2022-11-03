000880ba6bf44cbee6f22cc2f3909dfb16ac6ab1
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-11-03 15:16:50 +0100

    Add error logging in debug mode (#231)

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -4,7 +4,7 @@ import { RegisterRoutes } from "./modules/routes.gen.js";
 import { RegisterCronsJobs } from "./modules/cron.js";
 import { RegisterLogger } from "./modules/log.js";
 import { RegisterRequestParser } from "./modules/request.js";
-import { RegisterErrorCatcher } from "./modules/catch.js";
+import { RegisterErrorCatcher } from "./modules/error.js";
 import { RegisterDocs } from "./modules/docs.js";
 import { RegisterSecurityMiddleware } from "./modules/security.js";
 
--- packages/backend/src/modules/error.ts
+++ packages/backend/src/modules/error.ts
@@ -1,3 +1,5 @@
+import { Application, NextFunction, Request, Response } from "express";
+
 export class HttpError extends Error {
     status: number;
 
@@ -6,3 +8,16 @@ export class HttpError extends Error {
         this.status = status;
     }
 }
+
+export const RegisterErrorCatcher = (app: Application) => {
+    app.use((req: Request) => {
+        throw new HttpError(404, `"${req.url}" is not found.`);
+    });
+    
+    app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+        if (process.env.DEBUG === "true") {
+            console.log(err);
+        }
+        res.status(err.status || 500).send(err.message);
+    });
+};
