09af29eaaee89c3081bf433ad3baa148bc6c768d
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-11-04 18:34:22 +0100

    Add framework for `Terms of Service` and `Privacy Policy` pages (#233)
    * Add framework for `Terms of Service` and `Privacy Policy` pages
    
    * Lint fix
    
    * Lint fix

--- packages/frontend/src/components/footer.tsx
+++ packages/frontend/src/components/footer.tsx
@@ -1,14 +1,34 @@
 import "../styles/footer.css";
 import React, { Component } from "react";
+import ReactMarkdown from "react-markdown";
 
-export default class Footer extends Component<any, any> {
+interface IState {
+    legalText?: string;
+}
+
+export default class Footer extends Component<any, IState> {
     constructor(props: any) {
         super(props);
+        this.state = { };
         this.sendEmail = this.sendEmail.bind(this);
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
     }
 
     sendEmail() {
         window.location.href = "mailto:contact@jewel.cash";
+    }
+
+    openModal(page: string) {
+        return () => {
+            window.fetch(page)
+                .then(x => x.text())
+                .then(x => this.setState({ legalText: x }));
+        };
+    }
+
+    closeModal() {
+        this.setState({ legalText: undefined });
     }
 
     render() {
@@ -18,8 +38,15 @@ export default class Footer extends Component<any, any> {
                     <span>Copyright © 2022 jewel.cash</span>
                 </div>
                 <div className="footer-content">
-                    <span>Copyright © 2022 jewel.cash</span>
-                    <span className="footer-contact" onClick={this.sendEmail}>Contact</span>
+                    <span className="footer-left-long">Copyright © 2022 jewel.cash</span>
+                    <span className="footer-left-short">© 2022</span>
+                    <span className="footer-right" onClick={this.openModal("./privacy.md")}>Privacy Policy</span>
+                    <span className="footer-right" onClick={this.openModal("./terms.md")}>Terms of Service</span>
+                    <span className="footer-right" onClick={this.sendEmail}>Contact</span>
+                </div>
+                <div className="footer-legal-overlay" hidden={this.state.legalText == null} onClick={this.closeModal} />
+                <div className="footer-legal-content" hidden={this.state.legalText == null}>
+                    <ReactMarkdown>{this.state.legalText ?? ""}</ReactMarkdown>
                 </div>
             </div>
         );
