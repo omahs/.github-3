1d06c215a2597fbd652bf3c9f2e118b24ed1118b
Will Thieme <82029448+wjthieme@users.noreply.github.com>
2022-11-07 18:12:36 +0100

    Added styling to the overview tile (#239)

--- packages/frontend/src/components/overview.tsx
+++ packages/frontend/src/components/overview.tsx
@@ -7,12 +7,13 @@ interface IState {
     cumlative?: number;
     pending?: number;
     nextPaymentDate?: number;
+    isLoading: boolean;
 }
 
 class Overview extends Component<WithAuth0Props, IState> {
     constructor(props: WithAuth0Props) {
         super(props);
-        this.state = { };
+        this.state = { isLoading: true };
         this.setState = this.setState.bind(this);
     }
     
@@ -20,16 +21,26 @@ class Overview extends Component<WithAuth0Props, IState> {
         this.props.auth0.getAccessTokenSilently()
             .then(getDashboardOverview)
             .then(this.setState)
+            .then(() => this.setState({ isLoading: false }))
             .catch(console.log);
     }
 
     render() {
         return (
             <div className="overview">
-                Dashboard
-                <p>Cumlative: {this.state.cumlative?.toFixed(2)} USD</p>
-                <p>Pending: {this.state.pending?.toFixed(2)} USD</p>
-                <p>NextPayment: {this.state.nextPaymentDate?.relativeTo()}</p>
+                <div className="spinner" hidden={!this.state.isLoading}></div>
+                <div className="overview-content" hidden={this.state.isLoading}>
+                    <span className="overview-title">Dashboard</span>
+                    <span className="overview-sub">Cumlative earnings</span>
+                    <span className="overview-main">${this.state.cumlative?.toFixed(2)}</span>
+                    <span className="overview-sub">Pending balance</span>
+                    <span className="overview-main">${this.state.pending?.toFixed(2)}</span>
+                    <span className="overview-sub">Next payment<sup>*</sup></span>
+                    <span className="overview-main">{this.state.nextPaymentDate?.relativeTo()}</span>
+                    <span className="overview-disclaimer">
+                        <sup>*</sup>Pending balance will be paid out if it exceeds $100. Pending balance not paid out will carry over to the next payment date.
+                    </span>
+                </div>
             </div>
         );
     }
