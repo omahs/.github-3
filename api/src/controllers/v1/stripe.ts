ce747ef8ba63a7c2292761e71388f8474bae2ac3
Will <82029448+wjthieme@users.noreply.github.com>
2023-01-27 14:28:52 +0100

    Remove scheduled payments after a failed payment (#393)

--- api/src/controllers/v1/stripe.ts
+++ api/src/controllers/v1/stripe.ts
@@ -67,6 +67,7 @@ const handlers: Record<string, (data: object) => Promise<void>> = {
         if (payment.state !== PaymentState.initiated) { throw new HttpError(403, "payment already registered"); }
         payment.state = PaymentState.failed;
         await payment.save();
+        await Payment.deleteMany({ userId: payment.userId, state: PaymentState.scheduled });
 
         const mail = new Mail({
             userId: payment.userId,
