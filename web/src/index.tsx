b9f75a3b68f6b1173d1cb666f06daaeb5ffcddc1
Will <82029448+wjthieme@users.noreply.github.com>
2023-02-01 16:36:02 +0100

    Only index the production main page (#411)
    * Only index the production main page
    
    * Fix CodeQL alert

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { App } from "./components/app";
 import type { AuthorizationParams } from "@auth0/auth0-react";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { Helmet } from "react-helmet";
 
 const authUrl = process.env.REACT_APP_AUTH0_URL ?? "";
 const authId = process.env.REACT_APP_AUTH0_ID ?? "";
@@ -13,8 +14,13 @@ const authorizationParams: AuthorizationParams = {
 };
 
 const Root = (): ReactElement => {
+    const robots = window.location.hostname === "jewl.app" ? "index,follow" : "noindex,follow";
+
     return (
         <StrictMode>
+            <Helmet>
+                <meta name="robots" content={robots} />
+            </Helmet>
             <Auth0Provider domain={authUrl} clientId={authId} authorizationParams={authorizationParams}>
                 <App />
             </Auth0Provider>
