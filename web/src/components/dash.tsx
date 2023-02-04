629823657226d3f81b66ed8c979264d013cbba8c
Will <82029448+wjthieme@users.noreply.github.com>
2023-02-04 15:21:40 +0100

    Scaffold styling for the dash component (#426)

--- web/src/components/dash.tsx
+++ web/src/components/dash.tsx
@@ -1,21 +1,42 @@
 import "../styles/dash.css";
 import type { ReactElement } from "react";
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState, useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Timeline } from "./timeline";
-import { Checklist } from "./checklist";
 import { apiClient } from "../modules/network";
-
+import type { IPaymentResponse } from "jewl-core";
+import { Method } from "./method";
+import { Allocation } from "./allocation";
+import { Payment } from "./payment";
 
 export const Dash = (): ReactElement => {
+    const [lastPayment, setLastPayment] = useState<IPaymentResponse | null>(null);
     const [showConfirmationPopup, setShowConfirmationPopup] = useState(false);
     const { user, getAccessTokenSilently } = useAuth0();
+
+    useEffect(() => {
+        getAccessTokenSilently()
+            .then(async x => apiClient.getLastPayment(x))
+            .then(setLastPayment)
+            .catch(console.log);
+    }, []);
 
     let username = "";
     if (user?.email != null) {
         const parts = user.email.split("@");
         if (parts.length > 0) {
             username = parts[0];
+        }
+    }
+
+    let callout = "Start Dollar Cost averaging crypto in 3 easy steps!";
+    if (lastPayment != null) {
+        callout = "You're only a few steps away from reactivating your crypto investment!";
+        if (lastPayment.isActive) {
+            callout = "You're DCA investment will not auto-renew. Turn on auto-renew to keep investing!";
+        }
+        if (lastPayment.autoRenew) {
+            callout = `You're all set and investing ${lastPayment.amount.toFixed(0)} EUR every x weeks!`;
         }
     }
 
@@ -30,20 +51,20 @@ export const Dash = (): ReactElement => {
         setShowConfirmationPopup(!showConfirmationPopup);
     }, [showConfirmationPopup]);
 
-    // TODO: not show refund if no payment isActive
-
     return (
         <div className="dash">
-            <div className="dash-title">
-                Hi! {username}
-            </div>
-            <Checklist />
+            <div className="dash-title">Hi, {username}!</div>
+            <div className="dash-subtitle">{callout}</div>
+            <Method />
+            <Allocation />
+            <Payment />
             <Timeline />
             <div className="dash-footer">
-                Don&apos;t worry, we will also keep you updated on any purchase we make on your behalf via email!
-                If you&apos;ve changed your mind you can refund all your open orders
-                {" "}
-                <span className="dash-footer-link" onClick={toggleRefundPopup}>here</span>.
+                <span>Don&apos;t worry, we will also keep you updated on any purchase we make on your behalf via email! </span>
+                <span hidden={!(lastPayment?.isActive ?? false)}>
+                    <span>If you&apos;ve changed your mind you can request a refund for all your open orders </span>
+                    <span className="dash-footer-link" onClick={toggleRefundPopup}>here</span>.
+                </span>
             </div>
             <div className="dash-confirmation" hidden={!showConfirmationPopup}>
                 <button type="button" onClick={toggleRefundPopup}>Cancel</button>
