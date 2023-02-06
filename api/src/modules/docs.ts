2452b298abbff022d2fc0e23415135e787907c83
Will <82029448+wjthieme@users.noreply.github.com>
2023-02-06 14:18:35 +0100

    Fixed a couple of bugs on swagger page (#431)
    * Remove some styling from the swagger ui page
    
    * Fixed path in case the express server is not hosted at root
    
    * Fixed a couple of bugs on swagger page

--- api/src/modules/docs.ts
+++ api/src/modules/docs.ts
@@ -1,8 +1,21 @@
-import type { Application } from "express";
+import type { Application, Request, Response } from "express";
 import type { SwaggerUiOptions } from "swagger-ui-express";
-import { setup, serve } from "swagger-ui-express";
+import { serve, generateHTML } from "swagger-ui-express";
+
+const domain = process.env.AUTH0_AUDIENCE ?? "";
 
 export const RegisterDocs = (app: Application): void => {
+    const redirectScript = `
+        if (!window.location.pathname.endsWith("/")) {
+            window.location.pathname = window.location.pathname + "/";
+        }
+    `;
+
+    const additionalHead = [
+        "<meta name=\"robots\" content=\"noindex,follow\">",
+        `<script type="text/javascript">${redirectScript}</script>`
+    ];
+
     const css = `
         .topbar, .info, .servers, .servers-title, #operations-tag-default {
             display: none;
@@ -12,16 +25,21 @@ export const RegisterDocs = (app: Application): void => {
             justify-content: flex-start;
             align-self: center;
             flex: 1;
-            content: 'jewl.app';
+            content: '${domain}';
             font-family: sans-serif;
             font-size: 18px;
             font-weight: 700;
         }
     `;
+
     const specs: SwaggerUiOptions = {
         swaggerOptions: { url: "./swagger.gen.json" },
         customSiteTitle: "jewl.app",
         customCss: css
     };
-    app.use("/", serve, setup(undefined, specs));
+
+    const html = generateHTML(undefined, specs)
+        .replace("<meta charset=\"UTF-8\">", `<meta charset="UTF-8">${additionalHead.join("\n\t\r")}`);
+
+    app.use("/", serve, (_: Request, res: Response) => res.send(html));
 };
