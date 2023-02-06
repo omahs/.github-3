3fd66d99f28395b23a80c401762e08df94cf7e4f
Will <82029448+wjthieme@users.noreply.github.com>
2023-02-06 12:23:18 +0100

    Remove some styling from the swagger ui page (#430)
    * Remove some styling from the swagger ui page
    
    * Fixed path in case the express server is not hosted at root

--- api/src/modules/docs.ts
+++ api/src/modules/docs.ts
@@ -1,7 +1,27 @@
 import type { Application } from "express";
-import swaggerUi from "swagger-ui-express";
+import type { SwaggerUiOptions } from "swagger-ui-express";
+import { setup, serve } from "swagger-ui-express";
 
 export const RegisterDocs = (app: Application): void => {
-    const specs = { swaggerOptions: { url: "/swagger.gen.json" } };
-    app.use("/", swaggerUi.serve, swaggerUi.setup(undefined, specs));
+    const css = `
+        .topbar, .info, .servers, .servers-title, #operations-tag-default {
+            display: none;
+        }
+        .auth-wrapper:before {
+            display: flex;
+            justify-content: flex-start;
+            align-self: center;
+            flex: 1;
+            content: 'jewl.app';
+            font-family: sans-serif;
+            font-size: 18px;
+            font-weight: 700;
+        }
+    `;
+    const specs: SwaggerUiOptions = {
+        swaggerOptions: { url: "./swagger.gen.json" },
+        customSiteTitle: "jewl.app",
+        customCss: css
+    };
+    app.use("/", serve, setup(undefined, specs));
 };
