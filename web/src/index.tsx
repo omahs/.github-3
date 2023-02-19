9dc9c87329edd5c38935f0fc1ef00f8789c8871a
Will <82029448+wjthieme@users.noreply.github.com>
2023-02-19 10:47:56 +0100

    Added a note if you are on on unsupported orientation (#466)

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
 import "./styles/index.css";
 import type { ReactElement } from "react";
-import React, { StrictMode } from "react";
+import React, { StrictMode, useEffect, useMemo, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./components/app";
 import { Helmet } from "react-helmet";
@@ -9,12 +9,29 @@ import { Footer } from "./components/footer";
 import { Loading } from "./modules/loading";
 
 const Root = (): ReactElement => {
-    const robots = window.location.hostname === "jewl.app" ? "index,follow" : "noindex,follow";
+    const [supportedOrientation, setSupportedOrientation] = useState(true);
+    const robots = useMemo(() => window.location.hostname === "jewl.app" ? "index,follow" : "noindex,follow", []);
+
+    useEffect(() => {
+        const aspectChanged = (): void => {
+            setSupportedOrientation(window.innerHeight >= 512);
+        };
+        window.addEventListener("resize", aspectChanged);
+        aspectChanged();
+        return () => window.removeEventListener("resize", aspectChanged);
+    }, []);
+
+    const content = useMemo(() => {
+        if (supportedOrientation) {
+            return <Loading><Header /><App /><Footer /></Loading>;
+        }
+        return <div className="unsupported" hidden={supportedOrientation}>Please rotate your device to use jewl.app</div>;
+    }, [supportedOrientation]);
 
     return (
         <StrictMode>
             <Helmet><meta name="robots" content={robots} /></Helmet>
-            <Loading><Header /><App /><Footer /></Loading>
+            {content}
         </StrictMode>
     );
 };
